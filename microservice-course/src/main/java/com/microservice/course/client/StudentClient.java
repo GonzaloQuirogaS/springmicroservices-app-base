7a82d281e667319025d16fb27d89d37554fbe657
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2024-11-23 11:44:55 -0300

    6.4 Configuracion de clase StudentClient en microservicio Course.

--- microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -1,5 +1,6 @@
 package com.microservice.course.client;
 
+import com.microservice.course.presentation.dto.StudentDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,6 @@ import java.util.List;
 public interface StudentClient {
 
     @GetMapping("/search-by-course/{idCourse}")
-    List<?> findAllStudentsByCourse(@PathVariable Long idCourse);
+    List<StudentDto> findAllStudentsByCourse(@PathVariable Long idCourse);
 
 }
